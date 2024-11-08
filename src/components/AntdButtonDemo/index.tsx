bbf3ad72b03487ef14da0f3cbd2a8b0ae9bbae72
cruld <cruldra@gmail.com>
2024-11-08 18:09:55 +0800

    Deploy website - based on 7af0c0cb19f34b7d6e8a8a18ec9232fe9945d145

--- src/components/AntdButtonDemo/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, {FC} from 'react';
-import {Input} from "antd";
-
-type Props = {}
-const AntdButtonDemo: FC<Props> = ({}) => {
-    return   <Input placeholder="Basic usage" />;
-};
-
-export default AntdButtonDemo;
